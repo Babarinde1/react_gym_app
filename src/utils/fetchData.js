ef0333017c4272f589a210468e0ca1d4c11d5fcc
Babarinde1 <babjohn435@gmail.com>
2024-01-07 20:44:18 +0100

    little change

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_RAPID_API_KEY,
+    "X-RapidAPI-Key": "06c79aa807mshe7bd0f119cdeb8fp13f666jsn7b90778c58a8",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_RAPID_API_KEY,
+    "X-RapidAPI-Key": "fa96a40cdbmsh0727fa0a67a8113p1f8630jsn1565754b5284",
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
