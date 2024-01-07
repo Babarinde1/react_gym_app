c6e3262bce8eb7e746690569ea1d62b139bf7c5e
Babarinde1 <babjohn435@gmail.com>
2024-01-07 20:04:01 +0100

    changed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,9 @@
-export const exerciseOptions ={
-  method: 'GET',
+export const exerciseOptions = {
+  method: "GET",
   headers: {
-    'X-RapidAPI-Key': 'fa96a40cdbmsh0727fa0a67a8113p1f8630jsn1565754b5284',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": "06c79aa807mshe7bd0f119cdeb8fp13f666jsn7b90778c58a8",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
 };
 
 
